95d82baf4bf3bb95d9054f243ab27f6917445df5
D4KiR <d4kir92@gmail.com>
2022-09-19 13:32:44 +0200

    v0.3.0

--- ele/moneybar.lua
+++ ele/moneybar.lua
@@ -1,20 +1,70 @@
 
 local AddOnName, ImproveAny = ...
 
+local fontSize = 12
+
 IAMoneyBar = CreateFrame( "FRAME", "IAMoneyBar", UIParent )
-IAMoneyBar:SetSize( 180, 20 )
-IAMoneyBar:SetPoint( "BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -200, 100 )
+IAMoneyBar:SetSize( 240, 30 )
+IAMoneyBar:SetPoint( "BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", -240 - 10, 100 )
 
 function ImproveAny:InitMoneyBar()
-	IAMoneyBar.text = IAMoneyBar:CreateFontString(nil, "ARTWORK")
-	IAMoneyBar.text:SetFont(STANDARD_TEXT_FONT, 12, "THINOUTLINE")
-	IAMoneyBar.text:SetPoint("CENTER", IAMoneyBar, "CENTER", 0, 0)
-	IAMoneyBar.text:SetText(GetCoinTextureString(GetMoney()))
+	local oldMoney = GetMoney()
+	local ts = 0
+	local oldDir = ""
 
-	IAMoneyBar:RegisterEvent("PLAYER_MONEY")
-	IAMoneyBar:SetScript("OnEvent", function(self, ...)
-		IAMoneyBar.text:SetText( GetCoinTextureString( GetMoney() ) )
-	end)
+	IAMoneyBar.text = IAMoneyBar:CreateFontString( nil, "ARTWORK" )
+	IAMoneyBar.text:SetFont( STANDARD_TEXT_FONT, fontSize, "THINOUTLINE" )
+	IAMoneyBar.text:SetPoint( "TOP", IAMoneyBar, "TOP", 0, 0 )
+
+	IAMoneyBar.text2 = IAMoneyBar:CreateFontString( nil, "ARTWORK" )
+	IAMoneyBar.text2:SetFont( STANDARD_TEXT_FONT, fontSize, "THINOUTLINE" )
+	IAMoneyBar.text2:SetPoint( "BOTTOM", IAMoneyBar, "BOTTOM", 0, 0 )
+	
+	IAMoneyBar.Reset = CreateFrame( "Button", "ResetMoneyPerHour", IAMoneyBar, "UIPanelButtonTemplate" )
+	IAMoneyBar.Reset:SetSize( 100, 20 )
+	IAMoneyBar.Reset:SetPoint( "RIGHT", IAMoneyBar, 0, 0 )
+	IAMoneyBar.Reset:SetText( RESET )
+	IAMoneyBar.Reset:SetScript( "OnClick", function( self, ... )
+		ts = 0
+		oldMoney = GetMoney()
+	end )
+	IAMoneyBar.Reset:Hide()
+
+	IAMoneyBar:SetScript( "OnEnter", function( self ) 
+		IAMoneyBar.Reset:Show()
+	end )
+	IAMoneyBar:SetScript( "OnLeave", function( self ) 
+		if not MouseIsOver( IAMoneyBar.Reset ) then
+			IAMoneyBar.Reset:Hide()
+		end
+	end )
+	IAMoneyBar.Reset:SetScript( "OnLeave", function( self ) 
+		IAMoneyBar.Reset:Hide()
+	end )
+
+	function IAMoneyBar:MoneyThink()
+		local text = GetCoinTextureString( GetMoney(), fontSize )
+		IAMoneyBar.text:SetText( text )
+
+		local text2 = ""
+		if ts > 0 then
+			local totalMoney = GetMoney() - oldMoney
+			local mops = totalMoney / ts
+			local mopm = mops * 60
+			local moph = mopm * 60
+			if moph == 0 then
+				--
+			elseif moph >= 0 then
+				text2 = GetCoinTextureString( moph, fontSize ) .. "/" .. "h"
+			else
+				text2 = "-" .. GetCoinTextureString( math.abs( moph ), fontSize ) .. "/" .. "h"
+			end
+		end
+		IAMoneyBar.text2:SetText( text2 )		
+		ts = ts + 1
+		C_Timer.After( 1, IAMoneyBar.MoneyThink )
+	end
+	IAMoneyBar:MoneyThink()
 
 	if IABUILD ~= "RETAIL" then
 		GOLD_AMOUNT_SYMBOL = "|TInterface\\MoneyFrame\\UI-GoldIcon:12:12:2:0|t"
