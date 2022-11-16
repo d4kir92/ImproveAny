e27aff41b72280d7f66fe9109897440f442cf6bd
D4KiR <d4kir92@gmail.com>
2022-11-16 10:24:06 +0100

    v0.5.49

--- libs/dropdown.lua
+++ libs/dropdown.lua
@@ -1,50 +1,33 @@
 
---- Opts:
----     name (string): Name of the dropdown (lowercase)
----     parent (Frame): Parent frame of the dropdown.
----     items (Table): String table of the dropdown options.
----     defaultVal (String): String value for the dropdown to default to (empty otherwise).
----     changeFunc (Function): A custom function to be called, after selecting a dropdown option.
+local AddOnName, ImproveAny = ...
+
+local LibDD = LibStub:GetLibrary("LibUIDropDownMenu-4.0")
+
 function IACreateDropdown( opts )
-    local dropdown_name = "$parent_" .. opts["name"] .. "_dropdown"
-    local menu_items = opts["items"] or {}
-    local title_text = opts["title"] or ""
-    local dropdown_width = 0
-    local default_val = opts["defaultVal"] or ""
-    local change_func = opts["changeFunc"] or function (dropdown_val) end
+	local CB = LibDD:Create_UIDropDownMenu( opts.name, opts.parent )
+	LibDD:UIDropDownMenu_SetWidth( CB, 120 )
+	LibDD:UIDropDownMenu_SetText( CB, IAGV( opts.name, opts.defaultVal ) )
+	LibDD:UIDropDownMenu_Initialize(CB, function(self, level, menuList)
+		local tab = {}
+		for i, v in pairs( opts.items ) do
+			local info = LibDD:UIDropDownMenu_CreateInfo()
+			info.func = self.SetValue
+			info.text = v
+			info.arg1 = v
+			LibDD:UIDropDownMenu_AddButton(info)
+		end
+	end)
+	function CB:SetValue( newValue )
+		IASV( opts.name, newValue )
+		LibDD:UIDropDownMenu_SetText( CB, newValue )
+		CloseDropDownMenus()
+	end
+	CB:SetPoint("TOPLEFT", 0, -4)
+	
+	local text = CB:CreateFontString(nil, "ARTWORK")
+	text:SetFont(STANDARD_TEXT_FONT, 12, "THINOUTLINE")
+	text:SetPoint("LEFT", CB, "RIGHT", 0, 4)
+	text:SetText(IAGT(opts.title))
 
-    local dropdown = CreateFrame("Frame", dropdown_name, opts["parent"], "UIDropDownMenuTemplate")
-    local dd_title = dropdown:CreateFontString("dropdown_name_title", "OVERLAY", "GameFontNormal")
-    dd_title:SetPoint( "TOPLEFT", 0, 12 )
-
-    for _, item in pairs(menu_items) do -- Sets the dropdown width to the largest item string width.
-        dd_title:SetText(item)
-        local text_width = dd_title:GetStringWidth() + 30
-        if text_width > dropdown_width then
-            dropdown_width = text_width
-        end
-    end
-
-    UIDropDownMenu_SetWidth(dropdown, dropdown_width)
-    UIDropDownMenu_SetText(dropdown, default_val)
-    dd_title:SetText(title_text)
-
-    UIDropDownMenu_Initialize(dropdown, function(self, level, _)
-        local info = UIDropDownMenu_CreateInfo()
-        for key, val in pairs(menu_items) do
-            info.text = val
-            info.checked = false
-            info.menuList= key
-            info.hasArrow = false
-            info.func = function(b)
-                UIDropDownMenu_SetSelectedValue(dropdown, b.value, b.value)
-                UIDropDownMenu_SetText(dropdown, b.value)
-                b.checked = true
-                change_func(dropdown, b.value)
-            end
-            UIDropDownMenu_AddButton(info)
-        end
-    end)
-
-    return dropdown
+    return CB
 end
