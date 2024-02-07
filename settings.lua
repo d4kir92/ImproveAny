e62de436b2a29fc72b4275a035a8f8ca168843ec
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-02-07 07:46:10 +0100

    v0.9.34

--- settings.lua
+++ settings.lua
@@ -402,8 +402,8 @@ function ImproveAny:InitIASettings()
 		IASettings:Hide()
 	end
 
-	D4:SetVersion(AddonName, 136033, "0.9.33")
-	IASettings.TitleText:SetText(format("ImproveAny |T136033:16:16:0:0|t v|cff3FC7EB%s", "0.9.33"))
+	D4:SetVersion(AddonName, 136033, "0.9.34")
+	IASettings.TitleText:SetText(format("ImproveAny |T136033:16:16:0:0|t v|cff3FC7EB%s", "0.9.34"))
 	IASettings.CloseButton:SetScript(
 		"OnClick",
 		function()
