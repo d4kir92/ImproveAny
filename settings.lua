154fb339e180a8c821f03aaa4f831187dd13cfc1
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-10-08 11:31:02 +0200

    v0.9.183

--- settings.lua
+++ settings.lua
@@ -464,7 +464,7 @@ function ImproveAny:InitIASettings()
 		IASettings:Hide()
 	end
 
-	ImproveAny:SetVersion(136033, "0.9.182")
+	ImproveAny:SetVersion(136033, "0.9.183")
 	IASettings.TitleText:SetText(format("|T136033:16:16:0:0|t I|cff3FC7EBmprove|rA|cff3FC7EBny|r v|cff3FC7EB%s", ImproveAny:GetVersion()))
 	IASettings.CloseButton:SetScript(
 		"OnClick",
