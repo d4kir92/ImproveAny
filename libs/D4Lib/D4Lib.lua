8950e7d1948ef77b5ec6e8accb0a7f87c0743be8
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-11 13:57:56 +0100

    v0.9.201

--- libs/D4Lib/D4Lib.lua
+++ libs/D4Lib/D4Lib.lua
@@ -1287,8 +1287,12 @@ D4:After(
                 end
 
                 local function SetupRoleMenu(ownerRegion, rootDescription, contextData)
-                    if rootDescription.D4SetRole then return end
-                    rootDescription.D4SetRole = true
+                    if rootDescription.EnumerateElementDescriptions then
+                        for _, elementData in rootDescription:EnumerateElementDescriptions() do
+                            if elementData.isD4 then return end
+                        end
+                    end
+
                     local unit = contextData.unit
                     if unit == nil then return end
                     if not UnitIsPlayer(unit) then return end
@@ -1296,6 +1300,7 @@ D4:After(
                     local isLeader = UnitIsGroupLeader("player")
                     local isAssistant = UnitIsGroupAssistant("player")
                     local roleMenu = MenuUtil.CreateButton(D4:Trans("LID_CHOOSEROLE") .. " (by D4KiR)")
+                    roleMenu.isD4 = true
                     rootDescription:Insert(roleMenu, 2)
                     if UnitIsUnit(unit, "player") or isLeader or isAssistant then
                         roleMenu:SetEnabled(true)
