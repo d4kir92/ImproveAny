88e6e99dd800fdc0e392444fda0da79d190795e1
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-06-28 15:30:06 +0200

    refactor duability

--- ele/durabilityframe.lua
+++ ele/durabilityframe.lua
@@ -17,70 +17,69 @@ function ImproveAny:InitDurabilityFrame()
 			textrepaircosts:SetPoint("TOP", DurabilityFrame, "TOP", 0, 22)
 			textrepaircosts:SetText("")
 			textrepaircosts:SetTextColor(1.0, 1.0, 0.1)
-			local function Think()
-				pcall(
-					function()
-						local ccur = 0
-						local cmax = 0
-						for i = 0, 20 do
-							local curr, maxi = GetInventoryItemDurability(i)
-							if curr ~= nil and maxi ~= nil then
-								ccur = ccur + curr
-								cmax = cmax + maxi
-							end
-						end
-
-						local perc = 0
-						if cmax > 0 then
-							perc = ImproveAny:MathR(ccur / cmax * 100, 1)
-							if textperc ~= nil then
-								textperc:SetText(perc .. "%")
-								if perc > 50 then
-									textperc:SetTextColor(0.1, 1.0, 0.1)
-								elseif perc > 30 then
-									textperc:SetTextColor(1.0, 1.0, 0.1)
-								else
-									textperc:SetTextColor(1.0, 0.1, 0.1)
-								end
-							end
-						end
-
-						if not InCombatLockdown() then
-							if perc <= ImproveAny:IAGV("SHOWDURABILITYUNDER", 100) then
-								DurabilityFrame:Show()
-							else
-								DurabilityFrame:Hide()
-							end
-						end
-
-						if textrepaircosts then
-							local costs = 0
-							for i, v in pairs(IACHARSLOTS) do
-								local id = v:GetID()
-								if v.tt == nil then
-									v.tt = CreateFrame("GameTooltip", "DURA" .. i, DurabilityFrame)
-									v.tt:ClearLines()
-								end
-
-								if v.tt and v.tt.SetInventoryItem then
-									local cost = select(3, v.tt:SetInventoryItem("player", id))
-									costs = costs + cost
-								end
-							end
-
-							if costs > 0 then
-								textrepaircosts:SetText(GetCoinTextureString(costs))
-							else
-								textrepaircosts:SetText("")
-							end
-						elseif textrepaircosts then
-							textrepaircosts:SetText("")
-						end
-					end,
-					function(err)
-						print(err)
+			local function UpdateRepairCosts()
+				if not textrepaircosts then return end
+				local costs = 0
+				for i, v in pairs(IACHARSLOTS) do
+					local id = v:GetID()
+					if v.tt == nil then
+						v.tt = CreateFrame("GameTooltip", "DURA" .. i, DurabilityFrame)
+						v.tt:ClearLines()
 					end
-				)
+
+					if v.tt and v.tt.SetInventoryItem then
+						local cost = select(3, v.tt:SetInventoryItem("player", id))
+						costs = costs + cost
+					end
+				end
+
+				if costs > 0 then
+					textrepaircosts:SetText(GetCoinTextureString(costs))
+				else
+					textrepaircosts:SetText("")
+				end
+			end
+
+			local durFrame = CreateFrame("Frame")
+			ImproveAny:RegisterEvent(durFrame, "PLAYER_EQUIPMENT_CHANGED")
+			ImproveAny:RegisterEvent(durFrame, "PLAYER_ENTERING_WORLD")
+			ImproveAny:OnEvent(durFrame, UpdateRepairCosts, "DurabilityRepairCosts")
+			UpdateRepairCosts()
+
+			local function Think()
+				local ccur = 0
+				local cmax = 0
+				for i = 0, 20 do
+					local curr, maxi = GetInventoryItemDurability(i)
+					if curr ~= nil and maxi ~= nil then
+						ccur = ccur + curr
+						cmax = cmax + maxi
+					end
+				end
+
+				local perc = 0
+				if cmax > 0 then
+					perc = ImproveAny:MathR(ccur / cmax * 100, 1)
+					if textperc ~= nil then
+						textperc:SetText(perc .. "%")
+						if perc > 50 then
+							textperc:SetTextColor(0.1, 1.0, 0.1)
+						elseif perc > 30 then
+							textperc:SetTextColor(1.0, 1.0, 0.1)
+						else
+							textperc:SetTextColor(1.0, 0.1, 0.1)
+						end
+					end
+				end
+
+				if not InCombatLockdown() then
+					local threshold = ImproveAny:IAGV("SHOWDURABILITYUNDER", 100)
+					if perc <= threshold then
+						DurabilityFrame:Show()
+					else
+						DurabilityFrame:Hide()
+					end
+				end
 
 				ImproveAny:Debug("durabilityframe.lua: Think", "think")
 				ImproveAny:After(1, Think, "Think")
